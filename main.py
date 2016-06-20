3048d746fab1867b0d4c6085179a32bf1cd13f40
Dan Oneata <dan.oneata@gmail.com>
2016-06-21 01:04:21 +0300

    Add login and sample video

--- main.py
+++ main.py
@@ -6,27 +6,146 @@ import os
 import pdb
 import traceback
 
+from collections import namedtuple
+
 from logging import config
 
 from functools import wraps
 
 from flask import (
     Flask,
+    flash,
+    redirect,
     render_template,
     request,
     send_file,
     send_from_directory,
+    url_for,
+)
+
+from flask.ext.login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    current_user,
+    login_required,
+)
+
+from flask.ext.wtf import Form
+
+from wtforms import (
+    PasswordField,
+    SubmitField,
+    TextField,
+)
+
+from wtforms.validators import (
+    Email,
+    Required,
 )
 
 
 app = Flask(__name__)
+app.secret_key = 'imar'
+
 config.fileConfig('logger.conf')
 logger = logging.getLogger('video_annotation')
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User:
+    def __init__(self, email, password, id):
+        self.email = email
+        self.password = password
+        self.id = id 
+   
+    def get_id(self):
+        return self.id
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    def check_password(self, password):
+        return password == self.password
+
+
+# TODO Hash passwords
+users = [
+    User('eli@imar.ro', 'marinoiu', 1),
+    User('alin@imar.ro', 'popa', 2),
+]
+
+
+class LoginForm(Form):
+
+    email = TextField("Email",  [Required("Please enter your email address."), Email("Please enter your email address.")])
+    password = PasswordField('Password', [Required("Please enter a password.")])
+    submit = SubmitField("Log in")
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+
+    def validate(self):
+        # if not Form.validate(self):
+        #     return False
+        for user in users:
+            if user.email == self.email.data:
+                return user.check_password(self.password.data)
+        return False
+
 
 @app.route('/', methods=['GET'])
-def home():
-    return render_template('login.html')
+def index():
+    if current_user.is_authenticated:
+        return render_template("index.html")
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate():
+            for user in users:
+                if user.email == form.email.data:
+                    break
+            login_user(user)
+            flash('Welcome, {}!'.format(user.email), 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid email and/or password.', 'danger')
+            return render_template('login.html', form=form)
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
+
+
+@login_manager.user_loader
+def load_user(id):
+    for user in users:
+        if user.get_id() == id:
+            return user
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You were logged out.', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
 
 
 @app.route('/js/<path:path>')
