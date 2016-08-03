63c7415fd2f320de75f251cfc2f0be4cb0a95e7a
Dan Oneata <dan.oneata@gmail.com>
2016-08-03 17:38:12 +0300

    Add slider

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -248,5 +248,16 @@ $(document).ready(function() {
        
     }
 });
-    
+
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+    'max': 100
+    }
+});
+
 });
